162038f5ac0127ee4cc405d2a38a0b8c7c9ca78b
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2019-04-17 11:25:42 -0300

    adds safeguard for Blob in SSR, which does not exist

--- src/components/embed/index.js
+++ src/components/embed/index.js
@@ -132,7 +132,7 @@ export default class Embed extends React.Component {
     const { data } = this.props;
     const video = data.get("video");
 
-    const isBlob = video instanceof Blob;
+    const isBlob = window && video instanceof Blob;
     const isUrl = typeof video === "string";
     const embedUrl = isUrl && this.getEmbedUrl();
 
